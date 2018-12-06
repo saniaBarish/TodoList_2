04fb6ad935ca65c0a98f7b1ac8190138d4885e9e
Oleksandr <oleksandr@adminpdfs-MacBook-Air-4.local>
2018-12-06 21:31:26 +0200

    Updates

---
+++ precache-manifest.f885b50715d0323fb2413909b07e0e4f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d5220340599b49a5d60a",
+    "url": "/TodoList_2/static/js/runtime~main.d5220340.js"
+  },
+  {
+    "revision": "bed89b46d9c5584e2504",
+    "url": "/TodoList_2/static/js/main.bed89b46.chunk.js"
+  },
+  {
+    "revision": "b4c29c19280127ddb4ac",
+    "url": "/TodoList_2/static/js/1.b4c29c19.chunk.js"
+  },
+  {
+    "revision": "bed89b46d9c5584e2504",
+    "url": "/TodoList_2/static/css/main.8db19bea.chunk.css"
+  },
+  {
+    "revision": "d24e8e74685612be0b722bea6e00948f",
+    "url": "/TodoList_2/index.html"
+  }
+];
